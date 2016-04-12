0835646d02a9777b391c5ab7d3ac9f46f817b039
zerdzhong <zhongzhendong@youku.com>
2016-04-12 20:30:28 +0800

    add keyframe animation

--- CAExample/DetailViewController.swift
+++ CAExample/DetailViewController.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class DetailViewController: UIViewController {
     var animationType: AnimationType?
+    
+    override func viewDidLoad() {
+        if let animationType = self.animationType {
+            switch animationType {
+            case .Keyframe:
+                keyFrameAnimation()
+                break
+            }
+        }
+    }
+    
+    func keyFrameAnimation() {
+        let redView = UIView()
+        redView.frame = CGRectMake(100, 100, 100, 100)
+        redView.backgroundColor = UIColor.redColor()
+        
+        self.view.addSubview(redView)
+        
+        UIView.animateKeyframesWithDuration(2, delay: 0, options: [], animations: {
+            UIView.addKeyframeWithRelativeStartTime(0, relativeDuration: 0.25, animations: { 
+                redView.center.x += 200
+            })
+        }, completion: nil);
+    }
 }
